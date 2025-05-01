38aa1f22c8819fde0818b7597e3494bd2bdc162e
turo11192 <124545311+turo11192@users.noreply.github.com>
2025-04-30 19:53:34 -0600

    Add files via upload
    CRUD básico doble con madriles en una web api :)

---
+++ AprenderApis/AprenderApis/Controllers/HabilidadController.cs
@@ -0,0 +1,114 @@
+﻿using AprenderApis.Helpers;
+using AprenderApis.Models;
+using AprenderApis.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AprenderApis.Controllers
+{
+    [ApiController]
+    [Route("api/mandril/{idMandril}/[controller]")]
+    public class HabilidadController : ControllerBase
+    {        
+        [HttpGet]
+        public ActionResult<IEnumerable<Habilidad>> GetHabilidades(int idMandril)
+        {
+            var mandril = MandrilDataStore.Current.Mandriles.FirstOrDefault(x => x.Id == idMandril);
+
+            if (mandril == null)
+                return NotFound(Mensajes.Mandril.NotFound);
+
+            return Ok(mandril.Habilidades);
+        }
+
+        [HttpGet("{idHabilidad}")]
+        public ActionResult<Habilidad> GetHabilidad(int idMandril, int idHabilidad)
+        {
+            var mandril = MandrilDataStore.Current.Mandriles.FirstOrDefault(x => x.Id == idMandril);
+
+            if (mandril == null)
+                return NotFound(Mensajes.Mandril.NotFound);
+
+            var habilidad = mandril.Habilidades?.FirstOrDefault(h =>  h.Id == idHabilidad);
+
+            if (habilidad == null)
+                return NotFound(Mensajes.Habilidad.NotFound);
+
+            return Ok(habilidad);
+        }
+
+        [HttpPost]
+        public ActionResult<Habilidad> PostHabilidades(int idMandril, HabilidadInsert habilidadInsert)
+        {
+            var mandril = MandrilDataStore.Current.Mandriles.FirstOrDefault(x => x.Id == idMandril);
+
+            if (mandril == null)
+                return NotFound(Mensajes.Mandril.NotFound);
+
+            var habilidadExistente = mandril.Habilidades.FirstOrDefault(h => h.nombre == habilidadInsert.nombre);
+
+            if (habilidadExistente != null)
+                return BadRequest(Mensajes.Habilidad.NotFound);
+
+
+            var maxHabilidad = mandril.Habilidades.Any() ? mandril.Habilidades.Max(h => h.Id) : 0;
+
+            var habilidadNueva = new Habilidad()
+            {
+                Id = maxHabilidad + 1,
+                nombre = habilidadInsert.nombre,
+                potencia = habilidadInsert.potencia
+            };
+
+            mandril.Habilidades.Add(habilidadNueva);
+
+            return CreatedAtAction(nameof(GetHabilidad),
+                new { idMandril = idMandril, idHabilidad = habilidadNueva.Id },
+                habilidadNueva
+            );
+        }
+
+        [HttpPut("{idHabilidad}")]
+        public ActionResult<Habilidad> PutHabilidades(int idMandril, int idHabilidad, HabilidadInsert habilidadInsert)
+        {
+            var mandril = MandrilDataStore.Current.Mandriles.FirstOrDefault(x => x.Id == idMandril);
+
+            if (mandril == null)
+                return NotFound(Mensajes.Mandril.NotFound);
+
+            var habilidadExistente = mandril.Habilidades?.FirstOrDefault(h => h.nombre == habilidadInsert.nombre);
+
+            if (habilidadExistente == null)
+                return BadRequest(Mensajes.Habilidad.NotFound);
+
+
+            var habilidadMismoNombre = mandril.Habilidades?
+                .FirstOrDefault(h => h.Id != idHabilidad && h.nombre != habilidadInsert.nombre);
+
+            if (habilidadMismoNombre != null)
+                return BadRequest(Mensajes.Habilidad.NombreExistente);
+
+            habilidadExistente.nombre = habilidadInsert.nombre;
+            habilidadExistente.potencia = habilidadInsert.potencia;
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public ActionResult<Habilidad> DeleteHabilidades(int idMandril, int idHabilidad)
+        {
+            var mandril = MandrilDataStore.Current.Mandriles.FirstOrDefault(x => x.Id == idMandril);
+
+            if (mandril == null)
+                return NotFound(Mensajes.Mandril.NotFound);
+
+            var habilidadExistente = mandril.Habilidades?.FirstOrDefault(h => h.Id == idHabilidad);
+
+            if (habilidadExistente == null)
+                return BadRequest(Mensajes.Habilidad.NotFound);
+
+            mandril.Habilidades?.Remove(habilidadExistente);
+
+            return NoContent();
+        }
+    }
+}
---
+++ AprenderApis/AprenderApis/Controllers/MandrilController.cs
@@ -0,0 +1,79 @@
+﻿using AprenderApis.Helpers;
+using AprenderApis.Models;
+using AprenderApis.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AprenderApis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MandrilController : ControllerBase
+    {
+        //Mostrar todos los mandriles
+        [HttpGet]
+        public ActionResult<IEnumerable<Mandril>> GetMandriles()
+        {
+            return Ok(MandrilDataStore.Current.Mandriles);
+        }
+
+        //Mostrar mandril por id
+        [HttpGet("{idMandril}")]
+        public ActionResult<Mandril> GetMandriles(int idMandril)
+        {
+            var mandril = MandrilDataStore.Current.Mandriles.FirstOrDefault(x => x.Id == idMandril);
+
+            if (mandril == null)
+                return NotFound(Mensajes.Mandril.NotFound);
+            
+            return Ok(mandril);
+        }
+
+        //Agregar mandril
+        [HttpPost]
+        public ActionResult<Mandril> PostMandril(MandrilInsert mandrilInsert)
+        {
+            var maxMandrilId = MandrilDataStore.Current.Mandriles.Max(x => x.Id);
+
+            var mandrilNuevo = new Mandril()
+            {
+                Id = maxMandrilId + 1,
+                Nombre = mandrilInsert.Nombre,
+            };
+
+            MandrilDataStore.Current.Mandriles.Add(mandrilNuevo);
+
+            return CreatedAtAction(nameof(GetMandriles), 
+                new { idMandril = mandrilNuevo.Id },
+                mandrilNuevo
+            );
+        }
+
+        //Editar Mandril
+        [HttpPut("{idMandril}")]
+        public ActionResult<Mandril> PutMandril(int idMandril, MandrilInsert mandrilInsert)
+        {
+            var mandril = MandrilDataStore.Current.Mandriles.FirstOrDefault(x => x.Id == idMandril);
+
+            if (mandril == null)
+                return NotFound(Mensajes.Mandril.NotFound);
+
+            mandril.Nombre = mandrilInsert.Nombre;
+
+            return NoContent();
+        }
+
+        //Eliminar Mandril
+        [HttpDelete("{idMandril}")]
+        public ActionResult<Mandril> DeleteMandril (int idMandril)
+        {
+            var mandril = MandrilDataStore.Current.Mandriles.FirstOrDefault(x => x.Id == idMandril);
+
+            if (mandril == null)
+                return NotFound(Mensajes.Mandril.NotFound);
+
+            MandrilDataStore.Current.Mandriles.Remove(mandril);
+
+            return NoContent();
+        }
+    }
+}
